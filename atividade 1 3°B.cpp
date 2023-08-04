b648f688c369b70512ee3678323fd46f5cea9506
joaoplinio <124919664+joaoplinio@users.noreply.github.com>
2023-08-04 14:17:03 -0300

    Add files via upload

---
+++ atividade 1 3°B.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+	int vetor[8] = {999, 999, 999, 999, 999, 999, 999, 999};
+	int i;
+	
+	for (i = 0; i  < 8; i++) {
+		
+		printf("%d ", vetor[i]);
+	
+	}
+	
+	return 0;
+}
